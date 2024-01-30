47ed91dff8d0cb59cb0f62f328adec0c216842fd
kholoud001 <kholoud.sanak@gmail.com>
2024-01-30 14:16:45 +0100

    front

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/home', function () {
+    return view('home');
+})->name('home');
+
+
+Route::get('/product', function () {
+    return view('product');
+})->name('product');
+
+Route::get('/productForm', function () {
+    return view('productForm');
+})->name('productForm');
