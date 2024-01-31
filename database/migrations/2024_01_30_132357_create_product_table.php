3d20c9e332cf14e470444642afbf88472c36e2b7
kholoud001 <kholoud.sanak@gmail.com>
2024-01-31 11:48:58 +0100

    display list of products

--- database/migrations/2024_01_30_132357_create_product_table.php
+++ database/migrations/2024_01_30_132357_create_product_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table-> string('content');
             $table->float('price');
             $table->string('image');
-
             $table->timestamps();
         });
     }
