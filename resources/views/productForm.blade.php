adf672885c81d40e49c91e0c7a70609626d741a5
kholoud001 <kholoud.sanak@gmail.com>
2024-01-31 16:22:37 +0100

    CRUD

--- resources/views/productForm.blade.php
+++ resources/views/productForm.blade.php
@@ -12,7 +12,6 @@
 
 </head>
 <body>
-
 <div class="min-h-screen p-6 bg-gray-100 flex items-center justify-center">
     <div class="container max-w-screen-lg mx-auto">
         <div>
@@ -23,55 +22,61 @@
                     </div>
                     <!-- Product name -->
                     <div class="lg:col-span-2">
-                        <div id="productForms">
-                            <form id="addProductForm">
-                                <div class="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-5">
+
+                        <form method="post" action="{{ isset($id) ? route('updateProduct', $id) : route('storeProduct') }}">
+                            @csrf
+
+                            @if(isset($product))
+                                <input type="hidden" name="id" value="{{ $product->id }}">
+                            @endif
+
+                            <div class="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-5">
                                     <div class="md:col-span-3">
                                         <label for="title">Product Name</label>
-                                        <input type="text" id="title" placeholder="Enter your Product Name"
+                                        <input type="text" id="title" name="name" placeholder="Enter your Product Name"
                                                class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"/>
                                     </div>
 
                                     <div class="md:col-span-3">
                                         <label for="id">Price</label>
-                                        <input type="text" id="id" placeholder="Enter Your Price ....."
+                                        <input type="text" id="price" name="price" placeholder="Enter Your Price ....."
                                                class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"/>
                                     </div>
 
                                     <!-- Image URL -->
                                     <div class="md:col-span-3">
                                         <label for="image">Image URL</label>
-                                        <input type="text" id="image" placeholder="Enter Image URL ....."
+                                        <input type="text" name="image" id="image" placeholder="Enter Image URL ....."
                                                class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"/>
                                     </div>
                                     <!-- Product Description -->
                                     <div class="md:col-span-3">
                                         <label for="body">Product Description</label>
-                                        <input type="text" id="body" placeholder="Enter Your Product Description....."
+                                        <input type="text" id="body" name="content" placeholder="Enter Your Product Description....."
                                                class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"/>
                                     </div>
-
-
                                 </div>
-                            </form>
-                        </div>
-
-                        <!-- Submit button-->
-                        <div class="md:col-span-5 text-right">
-                            <div class="inline-flex items-end">
-                                <button id="save"
-                                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                                    Submit
-                                </button>
-
+                            <!-- Submit button-->
+                            <div class="md:col-span-5 text-right">
+                                <div class="inline-flex items-end">
+                                    <button  id="save"
+                                            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                        Submit
+                                    </button>
+                                </div>
                             </div>
-                        </div>
+                            </form>
+
+
+
+
+
                     </div>
                 </div>
             </div>
         </div>
 
-        <a href="{{ route('product') }}"  class="md:absolute  bottom-0 left-0 p-10 ">
+        <a href="{{ route('home') }}"  class="md:absolute  bottom-0 left-0 p-10 ">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none"
                  stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <path d="M19 12H5M12 19l-7-7 7-7"></path>
